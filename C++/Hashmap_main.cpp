f7138850b9292881919cb051e3f37de8fdcdd177
Jacob Runyan <jmrunyan@ucsc.edu>
2018-10-03 14:11:45 -0700

    Implemented the generic hashmap naively with a inefficient hash algo that just places everything in bucket 0.

--- C++/Hashmap_main.cpp
+++ C++/Hashmap_main.cpp
@@ -9,24 +9,23 @@
 using namespace std;
 
 #include "Hashmap.h"
+#include "Generic_Hashmap.h"
 
 int main(int argc, char** argv){
-    printf("Creating hashmap... ");
-    Hashmap* h = new Hashmap; 
-    printf("Done\n");
 
-    for(long long a=-4;a<15;++a){
-        h->insert(7,a*2);
-    }
-    h->remove(4);
-    h->remove(0);
-    h->remove(-1);
-    h->remove(15);
+    // Generic_Hashmap<int,int>* h = new Generic_Hashmap<int,int>; 
+    // for(long long a=-4;a<15;++a){ h->insert(7,a*2); }
+    // h->remove(4); h->remove(0);
+    // h->remove(-1); h->remove(15);
+    // h->print();
+    // printf("%d\n", h->get(7));
+    // h->update(7, 5555);
+    // printf("%d\n", h->get(9090));
 
+    Generic_Hashmap<std::string,std::string>* h = new Generic_Hashmap<string,string>; 
+    std::string key = "a", val = "b";
+    for(long long a=-4;a<15;++a){ h->insert(key,val); key+="a"; val+="b"; }
     h->print();
-    printf("%d\n", h->get(7));
-    h->update(7, 5555);
-    printf("%d\n", h->get(7));
 
     delete h;
     return 0;
--- C++/generic_hashmap.h
+++ C++/generic_hashmap.h
@@ -2,64 +2,174 @@
  * @author Jake Runyan
  * Hashmap.h
  * @desc Hashmap implementation in C++
- * @note Implementation MUST be done in the .h file for generic classes
- *     because of the way compilation around template classes is done.
  */
 
 #ifndef GENERIC_HASHMAP_H
 #define GENERIC_HASHMAP_H
 
 #include <vector>
+#include <iostream>
+#include <cmath>
 
-#define STARTING_BUCKET_SIZE 25
-#define STARTING_NUM_BUCKETS 10
+#define STARTING_BUCKET_SIZE 2
+#define STARTING_NUM_BUCKETS 2
+#define RESIZE_FACTOR 2
 
 template<class K, class V>
-class Hashmap {
+class Generic_Hashmap {
 private:
-    int bucket_size;
-    int num_buckets;
+    long long bucket_size;
+    long long num_buckets;
     float load_factor;
+    long long filled;
     std::vector<std::vector<std::pair<K,V>>>* map;
 public:
-    Hashmap(){
+    //Constructor to create a new Hashmap
+    Generic_Hashmap(){
         bucket_size = STARTING_BUCKET_SIZE;
         num_buckets = STARTING_NUM_BUCKETS;
         load_factor = 0.0;
-        std::vector<std::vector<std::pair<K,V>>> tmp = std::vector<std::vector<std::pair<K,V>>>();
+        filled = 0;
+        map = new std::vector<std::vector<std::pair<K,V>>>();
         for(int a=0; a<num_buckets; ++a){
-            std::vector<std::pair<K,V>> buck = std::vector<std::pair<K,V>>();
-            tmp.push_back(buck);
+            std::vector<std::pair<K,V>>* buck = new std::vector<std::pair<K,V>>();
+            map->push_back(*buck);
         }
-
-        map = &tmp;
     }
-    ~Hashmap(){
-        printf("Hashmap Deconstructor\n");
+    //Deconstructor deletes any dynamically allocated memory.
+    ~Generic_Hashmap(){
+        delete map;
+        map = nullptr;
     }
 
+    //Hashes key to return the bucket to retrieve/place in.
+    int hash(K key){
+        // return abs(key) % num_buckets;
+        return 0;
+    }
+
+    //resize and rehashes the table by increasing the table dimensions by a factor of RESIZE_FACTOR
+    //this is SLOW so try not to do it often
+    int rehash(){
+        bucket_size *= RESIZE_FACTOR;
+        num_buckets *= RESIZE_FACTOR;
+        std::vector<std::pair<K,V>>* allPairs = new std::vector<std::pair<K,V>>();
+        for(auto buck_itor = map->begin(); buck_itor != map->end(); ++buck_itor){
+            auto itor = (*buck_itor).begin();
+            while(itor != (*buck_itor).end()){
+                allPairs->push_back(*itor);
+                itor = (*buck_itor).erase(itor);
+                --filled;
+            }
+        }
+        while(map->size() < num_buckets){
+            std::vector<std::pair<K,V>>* buck = new std::vector<std::pair<K,V>>();
+            map->push_back(*buck);
+        }
+        for(std::pair<K,V> p : *allPairs){
+            insert(p.first,p.second);
+        }
+        load_factor = (float)filled / (bucket_size * num_buckets);
+        delete allPairs;
+        allPairs = nullptr;
+
+        return 0;
+    }
+
+    //Inserts a key/value pair into the hashtable. Returns 0 if unsuccessful
     int insert(K key, V value){
-        // map->at(0).push_back(std::pair<K,V>(key, value));
-
-
-        for(auto outside_itor = map->begin(); outside_itor != map->end(); ++outside_itor){
-            // for(auto itor = *outside_itor.begin(); itor != *outside_itor.end(); ++itor){
-            //     std::cout << "(" << *itor[0] << ", " << *itor[1] << ")" << std::endl;
-            // }
-            print("length: %d\n", *outside_itor.size());
+        if(exists(key)){
+            return 1;
         }
-
-
+        int insertion_index = hash(key);
+        if(map->at(insertion_index).size() >= bucket_size){
+            rehash(); 
+        }
+        std::pair<K,V>* p = new std::pair<K,V>;
+        p->first = key;
+        p->second = value;
+        map->at(insertion_index).push_back(*p);
+        ++filled;
+        load_factor = (float)filled / (bucket_size * num_buckets);
+        if(load_factor > 0.75){
+            rehash();
+        }
         return 0;
     }
-    V remove(K key){
-        return 0;
+
+    //Remove a key/value pair from the hashmap. Returns 1 if no such pair exists.
+    int remove(K key){
+        int insertion_index = hash(key);
+        for(auto itor = map->at(insertion_index).begin(); itor != map->at(insertion_index).end(); ++itor){
+            if(itor->first == key){
+                map->at(insertion_index).erase(itor);
+                return 0;
+            }
+        }
+        return 1;
     }
+
+    //Returns the value corresponding to the key in the hashmap. If not found, returns default constructor for value.
     V get(K key){
+        int insertion_index = hash(key);
+        for(auto itor = map->at(insertion_index).begin(); itor != map->at(insertion_index).end(); ++itor){
+            if(itor->first == key){
+                return itor->second;
+            }
+        }
+        return V();
+    }
+
+    //Updates the value corresponding to the key in the hashmap to val. If not found, returns 1.
+    int update(K key, V val){
+        int insertion_index = hash(key);
+        for(auto itor = map->at(insertion_index).begin(); itor != map->at(insertion_index).end(); ++itor){
+            if(itor->first == key){
+                itor->second = val;
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    bool exists(K key){
+        int insertion_index = hash(key);
+        for(auto itor = map->at(insertion_index).begin(); itor != map->at(insertion_index).end(); ++itor){
+            if(itor->first == key){
+                return 1;
+            }
+        }
         return 0;
     }
-    bool exists(K key){
-        return 0;
+
+    void print(){
+        for(auto buck_itor = map->begin(); buck_itor != map->end(); ++buck_itor){
+            std::vector<std::pair<K,V>> bucket = *buck_itor;
+            for(auto itor = bucket.begin(); itor != bucket.end(); ++itor){
+                std::pair<K,V> p = *itor;
+                // printf("(%d, %d)\n", p.first, p.second);
+                std::cout << "(" << p.first << "," << p.second << ")" << std::endl;
+            }
+        }
+        printf("Load Factor: %.4f (%lld/%lld)\n", load_factor, filled, (bucket_size*num_buckets));
+    }
+
+    void printInternals(){
+        int buck = 0;
+        for(auto buck_itor = map->begin(); buck_itor != map->end(); ++buck_itor){
+            std::vector<std::pair<K,V>> bucket = *buck_itor;
+            // printf("Bucket %d filled %lu/%lld\n", buck, bucket.size(), bucket_size);
+            printf("Bucket %d: ", buck);
+            std::cout << "Bucket " << buck << ": ";
+            for(auto itor = bucket.begin(); itor != bucket.end(); ++itor){
+                std::pair<K,V> p = *itor;
+                // printf("(%d, %d) ", p.first, p.second);
+                std::cout << "(" << p.first << "," << p.second << ") ";
+            }
+            // printf("\n");
+            std::cout << std::endl;
+            ++buck;
+        }
     }
 
 };
